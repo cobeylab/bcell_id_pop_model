2b036a262ef7ffbae5c10fac1c439cd32641a542
gnorthrup <gnorthrup@uchicago.edu>
2017-06-30 11:48:09 -0500

    Still can't get the non-masked model working properly

--- Code/Z+A2015.R
+++ Code/Z+A2015.R
@@ -4,7 +4,7 @@
 #Graham Northrup June 30
 
 NUMDAYS <- 60
-dt <- 0.05
+dt <- 0.01
 NUMSTEPS <- NUMDAYS/dt
 Hf <- rep(0, NUMSTEPS) #free antigen
 Hb <- rep(0, NUMSTEPS) #bound antigen
@@ -31,45 +31,48 @@ HbFUNC <- function(k,dH,A,Hf,Hb,t) { #evals Hb ODE
   return(k*A[t]*Hf[t] - dH*Hb[t])
 }
 
-BFUNC <- function(s,phi,B,Hf,t) { #B cell ODE
+BFUNCMask <- function(s,phi,B,Hf,t) { #B cell ODE
   return((s*B[t]*Hf[t])/(phi + Hf[t]))
 }
 
-BFUNCMask <- function(s,phi,B,Hf,Hb,t) { #B cell ODE
-  return((s*B[t]*(Hf[t]+Hb[t]))/(phi + (Hf[t]+Hb[t])))
+BFUNCNonMask <- function(s,phi,B,Hf,Hb,t) { #B cell ODE
+  H <- Hf[t] + Hb[t]
+  return((s*B[t]*H)/(phi + H))
 }
 
 AFUNC <- function(k,dA,a,B,Hf,A,t) { #Ab ODE
   return(a*B[t] - k*A[t]*Hf[t] - dA*A[t])
 }
 
-zero_floor <- function(value) {
-  if (is.na(value >= 0)) {
-    print('eval to NA')
-    print(value)
-    return(0)
-  } else if (value >= 0) {
-    return(value)
-  } else {
-    return(0)
-  }
-}
 
 #Forward Euler first because I want to make sure I'm
 #in the right ballpark on this
 
 for (i in 1:NUMSTEPS) {
   if (t[i] == 30) {
-    Hf[i+1] = Hf[i] + 1000
+    Hf[i+1] = 1000
   } else {
-    Hf[i+1] <- zero_floor(Hf[i] + dt*HfFUNC(k,dH,A,Hf,i))
+    Hf[i+1] <- Hf[i] + dt*HfFUNC(k,dH,A,Hf,i)
   }
-  Hb[i+1] <- zero_floor(Hb[i] + dt*HbFUNC(k,dH,A,Hf,Hb,i))
-  B[i+1] <- zero_floor(B[i] + dt*BFUNC(s,phi,B,Hf,i))
-  A[i+1] <- zero_floor(A[i] + dt*AFUNC(k,dA,a,B,Hf,A,i))
+  Hb[i+1] <- Hb[i] + dt*HbFUNC(k,dH,A,Hf,Hb,i)
+  B[i+1] <- B[i] + dt*BFUNCMask(s,phi,B,Hf,i)
+  A[i+1] <- A[i] + dt*AFUNC(k,dA,a,B,Hf,A,i)
+  if (Hf[i+1] < 0.0001) {
+    Hf[i+1] <- 0
+  }
+  if (Hb[i+1] < 0.0001) {
+    Hb[i+1] <- 0
+  } else if (Hb[i+1] > 1000) {
+    Hb[i+1] <- 1000
+  }
 
 }
 
-plot(t ,Hf,type='l',col='red',log = 'y', xlim=c(0,60),ylim=c(1,10000),xlab='Days',ylab='Count')
+plot(t ,Hf,type='l',col='red',log = 'y', xlim=c(0,60),ylim=c(1,100000000),xlab='Days',ylab='Count')
 lines(t,B,type='l',col='blue')
 lines(t,A,type='l',col='dark green')
+#lines(t,Hb,type='l', col='orange')
+
+#lines(t,Hf+Hb,type='l', col='purple')
+
+
