f1537d1d5b09a8bebe5c03bfc47138604ccd04de
gnorthrup <gnorthrup@uchicago.edu>
2017-08-30 11:58:51 -0500

    working with smooth K again, inferences better but not quite there yet

---
+++ Code/stan/smooth_K/rho_fit.R
@@ -0,0 +1,149 @@
+library('rstan')
+library('deSolve')
+library("ggplot2") #good data vis.
+library("reshape2") #data.frame reshaping
+
+setwd("~/Desktop/CobeyLab/bcell_id_pop_model/Code/stan/smooth_K")
+
+DUMMY_DATA <- 1
+NOISE <- 0
+PLOT <- 0
+STAN <- 1
+FIT_PLOT <- 0
+STD <- 1
+
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9",
+                "#009E73", "#F0E442", "#0072B2",
+                "#D55E00", "#CC79A7") #colorblind friendly colors
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+if (DUMMY_DATA){
+  Ags <- 66 #nM
+  AC50 <- 2
+  b_i <- 1/AC50
+  M <- 8000
+  Tp <- 0.9
+  t_peak <- 7#days
+  omega <- -3
+  tau <- 0.7
+  
+  Time <- 28
+  deltaT <- 7
+  t0 <- 0
+  nstep <- Time/deltaT
+  time <- seq(deltaT,Time,deltaT)
+  
+  state_vals <- c(B=1590,K=M*b_i)
+  params <- c(Ags=Ags,
+              AC50=AC50,
+              b_i=b_i,
+              M=M,
+              tau=tau,
+              t_peak=t_peak)
+  
+  model_func <- function(t, state, parms){
+    with(as.list(c(state, parms)),{
+      if (t <= t_peak) {
+        dK <- 0
+      } else {
+        dK <- omega*(t-t_peak)
+      }
+      dB <- ((tau)/AC50)*B*(1 - B/K)
+      list(c(dB,dK))
+    })
+  }
+  dummy <- ode(y = state_vals, times = time, func = model_func, parms = params)
+}
+
+if (NOISE) {
+  for (i in 1:length(dummy[,1])){
+    for (j in 2:length(dummy[1,])){
+      dummy[i,j] <- dummy[i,j] + rnorm(1,sd=STD)
+      if (dummy[i,j] < 0) {
+        dummy[i,j] <- 0
+      }
+    }
+  }
+}
+
+if (PLOT) {
+  df <- as.data.frame(dummy)
+  m_df <- melt(df, id=c('time'))
+  
+  p <- ggplot(m_df, aes(x = time, y = value, color = variable))
+  plot <- p + geom_line() + ylab("y") + ggtitle('Single Epitope dummy data')
+  plot <- plot + scale_color_manual(values=cbbPalette)
+  print(plot)
+}
+
+dummy_data <- dummy[,-1]
+
+if (STAN) {
+  estimates <- stan(file = 'rho_fit.stan',
+                    data = list (
+                      n  = nstep,
+                      B0 = state_vals[1],
+                      z  = dummy_data,
+                      t0 = t0,
+                      ts = time,
+                      Ags = Ags,
+                      AC50 = AC50,
+                      bi = b_i,
+                      tw = t_peak,
+                      omega = omega,
+                      tau = tau
+                    ),
+                    chains = 4,
+                    iter = 2000,
+                    warmup = 1000,
+                    refresh = 100,
+                    control = list(adapt_delta = 0.99,
+                                   max_treedepth = 10)
+  )
+  
+  print(estimates)
+}
+
+if(FIT_PLOT){
+  Time <- 28
+  deltaT <- 1
+  t0 <- 0
+  nstep <- Time/deltaT
+  time <- seq(deltaT,Time,deltaT)
+  samples <- extract(estimates, c('theta[1]','theta[2]'))
+  fit_M <- mean(samples[[1]])
+  fit_tau <- mean(samples[[2]])
+  state_vals <- c(fit_B=1590,fit_K=fit_M*b_i)
+  params <- c(Ags=Ags,
+              AC50=AC50,
+              b_i=b_i,
+              M=M,
+              tau=fit_tau,
+              t_peak=t_peak)
+  
+  fit_func <- function(t, state, parms){
+    with(as.list(c(state, parms)),{
+      if (t <= t_peak) {
+        dK <- 0
+      } else {
+        dK <- omega*(t-t_peak)
+      }
+      dB <- ((tau)/AC50)*fit_B*(1 - fit_B/fit_K)
+      list(c(dB,dK))
+    })
+  }
+  
+  fit <- ode(y = state_vals, times = time, func = fit_func, parms = params)
+  
+  fit_df <- as.data.frame(fit)
+  fit_m_df <- melt(fit_df, id=c('time'))
+  
+  both <- rbind(m_df,fit_m_df)
+  
+  p <- ggplot(both, aes(x = time, y = value, color = variable))
+  plot <- p + geom_line() + ylab("y") + ggtitle('Sa Fit for rho_m=6000 and tau=0.9')
+  plot <- plot + scale_color_manual(values=cbbPalette)
+  print(plot)
+}
---
+++ Code/stan/smooth_K/rho_fit.stan
@@ -0,0 +1,61 @@
+// saved as tau_fit.stan
+
+functions {
+  real[] model1(real t, real[] B, real[] theta, real[] x_r, int[] x_i){
+    real dXdt[2];
+    dXdt[1] = (((x_r[4])/x_r[2])*B[1])*(1 - (B[1]/(B[2])));
+    dXdt[2] = x_r[5]*(t);
+    return dXdt;
+  }
+}
+
+data {
+  int<lower=1> n; //number of observations
+  real<lower=0> B0; //initial state of B
+  real z[n,2]; //values of each observation
+  real t0; //time at B0
+  real ts[n]; //time at each observation
+  real<lower=0> Ags; //conc. of antigen for max specific binding
+  real<lower=0> AC50; //AC50 score
+  real<lower=0> bi; //bcells bound at Ags
+  real<lower=0> tw; //time at which K begins decreasing
+  real omega; //K waning parameter
+  real<lower=0> tau;
+}
+
+transformed data {
+  real x_r[5];
+  int x_i[0];
+  x_r[1] = Ags;
+  x_r[2] = AC50;
+  x_r[3] = bi;
+  x_r[4] = tau;
+  x_r[5] = omega;
+}
+
+parameters {
+  real m;
+  real<lower=0> sigma[1];
+}
+
+transformed parameters {
+  real theta[1];
+  theta[1] = 10000 + 2000*m;
+}
+
+model {
+  real z_hat[n,2];
+  real B[2];
+  B[1] = B0;
+  B[2] = theta[1]*bi;
+  sigma[1] ~ student_t(3,0,1);
+  // sigma[2] ~ student_t(3,0,1);
+  m ~ normal(0,1);
+  // theta[1] ~ normal(1,1);
+  z_hat = integrate_ode_rk45(model1, B, t0, ts, theta, x_r, x_i);
+  for (i in 1:n){
+    z[i,1] ~ normal(z_hat[i,1],sigma[1]);
+    // z[i,2] ~ normal(z_hat[i,2],sigma[2]);
+  }
+
+}
